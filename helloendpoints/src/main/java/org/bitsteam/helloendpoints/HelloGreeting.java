a9028450126c28db94d914ed56bbe2cbdeeda401
colbyjax <colbyjax@zoho.com>
2015-03-24 20:42:13 -0400

    Initial Commit

---
+++ helloendpoints/src/main/java/org/bitsteam/helloendpoints/HelloGreeting.java
@@ -0,0 +1,20 @@
+package org.bitsteam.helloendpoints;
+
+public class HelloGreeting {
+
+  public String message;
+
+  public HelloGreeting() {};
+
+  public HelloGreeting(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+}
